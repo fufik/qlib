f71b1de1598eaa4ed98f73a28d5fa0a84d92d35d
fufik <ivaiva1999ivaiva@gmail.com>
2019-12-21 19:17:23 +0000

    Critical fix, Pauli-Y matrix

--- qlib/matrix.py
+++ qlib/matrix.py
@@ -3,7 +3,7 @@ import cmath
 import math
 EYE = np.eye(2,dtype=complex)
 X = np.array([[0,1],[1,0]],dtype=complex)
-Y = np.array([[0,1j],[1j,0]],dtype=complex)
+Y = np.array([[0,-1j],[1j,0]],dtype=complex)
 Z = np.array([[1,0],[0,-1]],dtype=complex)
 H = (1/cmath.sqrt(2)) * np.array([[1,1],[1,-1]],dtype=complex)
 
